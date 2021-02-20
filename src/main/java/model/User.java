0491cba05f02e7f34b20c60c517fd73e0126581f
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-02-20 17:17:47 +0700

    create class user

---
+++ src/main/java/model/User.java
@@ -0,0 +1,67 @@
+package model;
+
+public class User {
+    private String account;
+    private String password;
+    private String email;
+    private String avatar;
+    private int phoneNumber;
+    private String address;
+
+    public User(String account, String password, String email, String avatar, int phoneNumber, String address) {
+        this.account = account;
+        this.password = password;
+        this.email = email;
+        this.avatar = avatar;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
