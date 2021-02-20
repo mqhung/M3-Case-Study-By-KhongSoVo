0d1cf0d25c0bb2fa27e08bdf1c8dcacd2fd7259c
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-02-20 17:30:33 +0700

    create class RealativeStatus

---
+++ src/main/java/model/RelativeStatus.java
@@ -0,0 +1,17 @@
+package model;
+
+public class RelativeStatus {
+    private String status;
+
+    public RelativeStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
